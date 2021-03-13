f2988adc34239ecbec02ef23b6ac5fe31537dcda
Drew Brumbach <Drew_Brumbach1@baylor.edu>
2021-03-13 09:49:09 -0600

    new defs in rungame, added keydown right

--- alien_invasion.py
+++ alien_invasion.py
@@ -25,17 +25,27 @@ class AlienInvasion:
     def run_game(self):
         """Start the main loop for the game."""
         while True:
-            # Watch for keyboard and mouse event.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-
-            # redraw the screen during each pass through the loop.
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
+            self._check_events()
+            self._update_screen()
 
             # Make the most recently drawn screen visible.
             pygame.display.flip()
+
+    def _check_events(self):
+        """Respond to keypresses and mouse event."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    # Move ship to the right.
+                    self.ship.rect.x += 1
+
+    def _update_screen(self):
+        """"Update images on the screen, and flip to the new screen"""
+        # redraw the screen during each pass through the loop.
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
 
 
 if __name__ == "__main__":
