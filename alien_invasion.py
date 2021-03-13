0d413c4e40b3e66236aad91cf7acdbdb23e7db13
Drew Brumbach <Drew_Brumbach1@baylor.edu>
2021-03-13 10:36:44 -0600

    full screen and quit

--- alien_invasion.py
+++ alien_invasion.py
@@ -15,6 +15,10 @@ class AlienInvasion:
         pygame.init()
         self.settings = Settings()
 
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
+
         self.screen = pygame.display.set_mode(
             (self.settings.screen_width, self.settings.screen_height)
         )
@@ -56,6 +60,8 @@ class AlienInvasion:
             self.ship.moving_right = False
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
+        elif event.key == pygame.K_q:
+            sys.exit()
 
     def _update_screen(self):
         """"Update images on the screen, and flip to the new screen"""
