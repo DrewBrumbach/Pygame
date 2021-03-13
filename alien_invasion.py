dd4627a92389a31db1938bf9dc01d68bfd4cda29
Drew Brumbach <Drew_Brumbach1@baylor.edu>
2021-03-12 22:38:42 -0600

    draw ship to ai, bug fixes on ai and settings

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,20 +4,23 @@ import pygame
 
 from settings import Settings
 
+from ship import Ship
+
+
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
 
     def __init__(self):
         """Initialize the game, and create game resources."""
         pygame.init()
-        self.settings() = Settings()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_model(
-            (self.settings.screen_width,self.settings.screen_height))
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height)
+        )
         pygame.display.set_caption("Alien_Invasion")
 
-        # Set the background color.
-        self.bg_color = (230, 230, 230)
+        self.ship = Ship(self)
 
     def run_game(self):
         """Start the main loop for the game."""
@@ -29,6 +32,7 @@ class AlienInvasion:
 
             # redraw the screen during each pass through the loop.
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
 
             # Make the most recently drawn screen visible.
             pygame.display.flip()
--- settings.py
+++ settings.py
@@ -5,5 +5,5 @@ class Settings:
         """Initialize the game's settings."""
         # Screen settings
         self.screen_width = 1200
-        self.screen_height = 800
-        self.bg_color(230, 230, 230)
+        self.screen_height = 600  # can't see ship when set to 800
+        self.bg_color = (230, 230, 230)
