99997e2f77bbcc9c227651006b12b1e171a62130
Drew Brumbach <Drew_Brumbach1@baylor.edu>
2021-03-13 19:06:31 -0600

    fleet advances

--- alien.py
+++ alien.py
@@ -9,7 +9,7 @@ class Alien(Sprite):
         """initialize the alien and set its starting position."""
         super().__init__()
         self.screen = ai_game.screen
-        self.settings.ai_game.settings
+        self.settings = ai_game.settings
 
         # load the alien image and set its rect attribute
         self.image = pygame.image.load("images/alien.bmp")
@@ -26,11 +26,10 @@ class Alien(Sprite):
         """Return True if alien is at the edge of the screen."""
         screen_rect = self.screen.get_rect()
 
-        if self.rect_right >= screen_rect.right or self.rect.left <= 0:
+        if self.rect.right >= screen_rect.right or self.rect.left <= 0:
             return True
-            
 
-    def update(self)
+    def update(self):
         """Move the alien to the right"""
-        self.x += (self.settings.alien_speed * self.settings.fleet_direction)
+        self.x += self.settings.alien_speed * self.settings.fleet_direction
         self.rect.x = self.x
